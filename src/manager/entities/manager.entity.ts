6e74aeb864d0c08cd76eee37f1e5a5d291569f1f
NANO1903 <fernando.beristain.alvarez@gmail.com>
2025-09-30 10:45:59 -0600

    User Relations to Manager and Employees

--- src/manager/entities/manager.entity.ts
+++ src/manager/entities/manager.entity.ts
@@ -1,5 +1,6 @@
-import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Location } from '../../locations/entities/location.entity';
+import { User } from 'src/auth/entities/user.entity';
 
 @Entity()
 export class Manager {
@@ -22,4 +23,10 @@ export class Manager {
 
     @OneToOne(() => Location)
     location: Location;
+
+    @OneToOne(() => User)
+    @JoinColumn({
+        name: "userId"
+    })
+    usre: User;
 }
