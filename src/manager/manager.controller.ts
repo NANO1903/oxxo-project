d3231faf189fcc59dc40427095ce27500dcefd87
NANO1903 <fernando.beristain.alvarez@gmail.com>
2025-10-01 20:38:22 -0600

    Auth for Routes

--- src/manager/manager.controller.ts
+++ src/manager/manager.controller.ts
@@ -2,31 +2,38 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ManagerService } from './manager.service';
 import { CreateManagerDto } from './dto/create-manager.dto';
 import { UpdateManagerDto } from './dto/update-manager.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ROLES } from 'src/auth/constants/roles.constants';
 
 @Controller('manager')
 export class ManagerController {
   constructor(private readonly managerService: ManagerService) {}
 
+  @Auth()
   @Post()
   create(@Body() createManagerDto: CreateManagerDto) {
     return this.managerService.create(createManagerDto);
   }
-
+  
+  @Auth()
   @Get()
   findAll() {
     return this.managerService.findAll();
   }
-
+  
+  @Auth()
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.managerService.findOne(id);
   }
-
+  
+  @Auth(ROLES.MANAGER)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateManagerDto: UpdateManagerDto) {
     return this.managerService.update(id, updateManagerDto);
   }
-
+  
+  @Auth()
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.managerService.remove(id);
--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -5,42 +5,49 @@ import { UpdateProviderDto } from './dto/update-provider.dto';
 import { UserData } from 'src/auth/decorators/user.decorator';
 import { User } from 'src/auth/entities/user.entity';
 import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ROLES } from 'src/auth/constants/roles.constants';
 
 @Controller('providers')
 export class ProvidersController {
   constructor(private readonly providersService: ProvidersService) {}
 
+  @Auth(ROLES.MANAGER)
   @Post()
   create(@Body() createProviderDto: CreateProviderDto) {
     return this.providersService.create(createProviderDto);
   }
   
-  @Auth("Admin")
+  @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
   @Get()
   findAll(@UserData() user: User) {
     if(user.userRoles.includes("Employee")) throw new UnauthorizedException("No estás autorizado, sólo admins");
     return this.providersService.findAll();
-  }
-
+  } 
+  
+  
+  @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
   @Get('/provider-name/:providerName')
   findByName(@Param('providerName') providerName: string) {
     const provider = this.providersService.findByName(providerName);
     if(!provider) throw new NotFoundException();
     return provider;
   }
-
+  
+  @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
   @Get(':id')
   findOne(@Param('id') id: string) {
     const provider = this.providersService.findOne(id);
     if(!provider) throw new NotFoundException();
     return provider;
   }
-
+  
+  @Auth(ROLES.MANAGER)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProviderDto: UpdateProviderDto) {
     return this.providersService.update(id, updateProviderDto);
   }
-
+  
+  @Auth(ROLES.MANAGER)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.providersService.remove(id);
